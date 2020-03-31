ea8bf6b2ca5f20f54261a25b637b26a0a48c649e
Harley McDowell <hmcdowell95@gmail.com>
2020-03-31 21:40:15 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,40 @@
+require 'pry'
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+  
+  def initialize (id: id, name: name, type: type, db: db)
+    @id = id
+    @name = name 
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    sql = <<-SQL
+    INSERT INTO pokemon (name, type)
+    VALUES (?,?)
+    SQL
+    db.execute(sql, name, type)
+  end
+  
+  def self.find(id, db)
+    sql = <<-SQL
+    SELECT * FROM pokemon
+    SQL
+    a = id - 1
+    b = db.execute(sql)
+    z = b[a]
+    self.new_from_db(z)
+  end
+  
+  def self.new_from_db(array)
+    new_poke = self.new
+    new_poke.id = array[0]
+    new_poke.name = array[1]
+    new_poke.type = array[2]
+    new_poke.db = @db
+    new_poke
+  end
+  
+  
 end
